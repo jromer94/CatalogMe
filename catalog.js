bef6a7e928ca1348869d44ceaf248d781d2fcc35
Josh <jromer94@gmail.com>
2015-10-03 19:41:50 -0400

    made signup functions take user iput

--- catalog.js
+++ catalog.js
@@ -3,26 +3,31 @@ var request = require('request');
 
 prompt.start();
 
-prompt.get(['firstname', 'lastname', 'address1', 'address2', 'city', 'state', 'zipcode'], function (err, result) {
+prompt.get(['firstname', 'lastname', 'address1', 'address2', 'city', 'state', 'zip', 'phone'], function (err, result) {
+    console.log(result);
 
-    oldiesSignUp();
+    //blairSignUp(result)
+    //monroeSignUp(result)
+    //ginnysSignUp(result)
+    //momaSignUp(result)
+    //oldiesSignUp(result)
 });
 
-function blairSignUp(){
+function blairSignUp(result){
     var r = request.post(
         'https://www.blair.com/catalog/request_catalog_command.cmd',
          
-        { form: { address1: 'Test',
-                  address2: '',
-                  city: "New Brunswick",
+        { form: { address1: result['address1'],
+                  address2: result['address2'],
+                  city: result['city'],
                   company: '',
                   emailAddress: '',
                   emailFormat: 'html',
-                  firstName: 'Test',
+                  firstName: result['firstname'], 
                   form_state: 'requestCatalogForm',
-                  lastName: "test",
-                  state: 'NJ',
-                  zipCode: '08901'}, followAllRedirects: true}, 
+                  lastName: result['lastname'],
+                  state: result['state'],
+                  zipCode: result['zip']}, followAllRedirects: true}, 
         function (error, response, body) {
             console.log(response.request.uri);
         }
@@ -30,81 +35,81 @@ function blairSignUp(){
 
 }
 
-function monroeSignUp(){
+function monroeSignUp(result){
     var r = request.post(
         'https://www.monroeandmain.com/catalog/request_catalog.cmd',
          
-        { form: { address1: '12 high street',
-                  address2: '',
+        { form: { address1: result['address1'],
+                  address2: result['address2'],
                   addressValidation: 'true',
                   catalog: 'W',
-                  city: "New Brunswick",
+                  city: result['city'],
                   countryCode: 'US',
                   emailAddress: '',
-                  firstName: 'John',
+                  firstName: result['firstname'],
                   form_state: 'requestCatalogForm',
-                  lastName: "test",
+                  lastName: result['lastname'],
                   needPhoneValidation: 'false',
                   phone: '',
-                  state: 'NJ',
+                  state: result['state'],
                   title: '',
                   validateZipCode: 'Y',
                   x: '0',
                   y: '0',
-                  zipCode: '08901'}, followAllRedirects: true}, 
+                  zipCode: result['zip']}, followAllRedirects: true}, 
         function (error, response, body) {
             console.log(response.request.uri);
         }
     );}
 
 //same as monroe just diff cat
-function ginnysSignUp(){
+function ginnysSignUp(result){
     var r = request.post(
         'https://www.ginnys.com/catalog/request_catalog.cmd',
          
-        { form: { address1: '12 high street',
-                  address2: '',
+        { form: { address1: result['address1'],
+                  address2: result['address2'],
                   addressValidation: 'true',
-                  catalog: 'G,J',
-                  city: "New Brunswick",
+                  catalog: 'W',
+                  city: result['city'],
                   countryCode: 'US',
                   emailAddress: '',
-                  firstName: 'John',
+                  firstName: result['firstname'],
                   form_state: 'requestCatalogForm',
-                  lastName: "test",
+                  lastName: result['lastname'],
                   needPhoneValidation: 'false',
                   phone: '',
-                  state: 'NJ',
+                  state: result['state'],
                   title: '',
                   validateZipCode: 'Y',
                   x: '0',
                   y: '0',
-                  zipCode: '08901'}, followAllRedirects: true}, 
+                  zipCode: result['zip']}, followAllRedirects: true}, 
         function (error, response, body) {
             console.log(response.request.uri);
         }
     );}
 
 //probably works
-function momaSignUp() {
+function momaSignUp(result) {
     var r = request.post(
         'http://www.momastore.org/webapp/wcs/stores/servlet/AjaxRequestCatalogCmd',
          
-        { form: { address1: '32 High Street', 
-                  address2: '', 
+        { form: { address1: result['address1'], 
+                  address2: result['address2'], 
                   catalogId: '10451', 
-		  city: "New Brunswick", 
+		  city: result['city'], 
                   country: 'US',
                   email: '', 
-                  firstName: 'John', 
+                  firstName: result['firstname'], 
 		  langId: '-1',
-                  lastName: "Ryan", 
+                  lastName: result['lastname'], 
                   origin: '5D',
                   requesttype: 'ajax',
-                  state: 'NJ', 
+                  state: result['state'], 
                   storeId: '10001',
                   successURL: 'MoMARequestCatalogSuccessView',
-                  zip: '08901'},
+                  zip: result['zipcode']},
           followAllRedirects: true}, 
         function (error, response, body) {
             console.log(response.request.uri);
@@ -112,22 +117,22 @@ function momaSignUp() {
     );
 }
 
-function oldiesSignUp() {
+function oldiesSignUp(result) {
     var r = request.post(
         'http://www.oldies.com/catalog-request/',
          //capitalize city
         { form: { address: '0',
-                  address1: '32 HIGH STREET', 
-                  address2: '', 
+                  address1: result['address1'], 
+                  address2: result['address2'], 
                   catalog: 'movie,cd,book,45,gift', 
-		  city: "NEW BRUNSWICK", 
-                  fullname: 'John Ryan', 
+		  city: result[city], 
+                  fullname: result[firstname] + " " + result[lastname], 
                   fuseaction: 'CatalogRequest.Save', 
                   int: 'false',
                   referrer: '',
-                  state: 'NJ', 
+                  state: result[state], 
                   verify: 'true',
-                  zip: '08901'},
+                  zip: result['zip']},
           followAllRedirects: true}, 
         function (error, response, body) {
             console.log(response.request.uri);
